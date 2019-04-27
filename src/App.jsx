f592d300fe3ba7a75cb10d0230c99b029224cdab
Andrew Godley <agdevlabs@gmail.com>
2019-04-27 23:46:18 +0100

    lift state of images to allow for use in own hand section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,56 @@ import MySection from './Components/mySection.jsx';
 import './App.css';
 
 
+import cardBack from './Images/cardBack.png';
+import cardFront from './Images/card.png';
+import club from './Images/suits/club.png';
+import diamond from './Images/suits/diamond.png';
+import heart from './Images/suits/heart.png';
+import spade from './Images/suits/spade.png';
+
+import one from './Images/numbers/one.png'
+import two from './Images/numbers/two.png'
+import three from './Images/numbers/three.png'
+import four from './Images/numbers/four.png'
+import five from './Images/numbers/five.png'
+import six from './Images/numbers/six.png'
+import seven from './Images/numbers/seven.png'
+import eight from './Images/numbers/eight.png'
+import nine from './Images/numbers/nine.png'
+import ten from './Images/numbers/ten.png'
+import jack from './Images/numbers/Jack.png'
+import queen from './Images/numbers/Queen.png'
+import king from './Images/numbers/King.png'
+
+
 class App extends Component {
+
+  suits2 = {
+    club: club,
+    diamond: diamond,
+    heart: heart,
+    spade: spade
+  }
+  numbers2 ={
+      one:one,
+      two:two,
+      three:three,
+      four:four,
+      five:five,
+      six:six,
+      seven:seven,
+      eight:eight,
+      nine:nine,
+      ten:ten,
+      jack:jack,
+      queen:queen,
+      king:king
+  }
 
   render() {
     return (
       <div className="App">
-      <PokerTable/>
+      <PokerTable suits = {this.suits2} numbers={this.numbers2}/>
         <div className="bottom">
           <MySection/>
 
--- src/Components/table.jsx
+++ src/Components/table.jsx
@@ -6,50 +6,12 @@ import NumberPopper from './number-popper';
 
 import cardBack from '../Images/cardBack.png';
 import cardFront from '../Images/card.png';
-import club from '../Images/suits/club.png';
-import diamond from '../Images/suits/diamond.png';
-import heart from '../Images/suits/heart.png';
-import spade from '../Images/suits/spade.png';
 
-import one from '../Images/numbers/one.png'
-import two from '../Images/numbers/two.png'
-import three from '../Images/numbers/three.png'
-import four from '../Images/numbers/four.png'
-import five from '../Images/numbers/five.png'
-import six from '../Images/numbers/six.png'
-import seven from '../Images/numbers/seven.png'
-import eight from '../Images/numbers/eight.png'
-import nine from '../Images/numbers/nine.png'
-import ten from '../Images/numbers/ten.png'
-import jack from '../Images/numbers/Jack.png'
-import queen from '../Images/numbers/Queen.png'
-import king from '../Images/numbers/King.png'
 
 import '../Styles/table.css'
 
 const pokerTable = props => {
 
-    const suits = {
-        club: club,
-        diamond: diamond,
-        heart: heart,
-        spade: spade
-    }
-    const numbers ={
-        one:one,
-        two:two,
-        three:three,
-        four:four,
-        five:five,
-        six:six,
-        seven:seven,
-        eight:eight,
-        nine:nine,
-        ten:ten,
-        jack:jack,
-        queen:queen,
-        king:king
-    }
     const [selectedCard, setSelectedCard] = React.useState('')
     const [selectedSuit, setSelectedSuit] = React.useState('')
 
@@ -95,9 +57,9 @@ const pokerTable = props => {
         
         mergeImages([
           {src: cardFront, x: 0, y: 0},
-          {src: suits[setSuit], x: 62, y: 100},
-          {src: numbers[setNum],x:25,y:20},
-          {src: numbers[setNum],x:195,y:280}
+          {src: props.suits[setSuit], x: 62, y: 100},
+          {src: props.numbers[setNum],x:25,y:20},
+          {src: props.numbers[setNum],x:195,y:280}
         ])
           .then(b64 => {
             document.querySelector('#'+photoID).src = b64
